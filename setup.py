e40395b661cc717fcd4fa69c943ac1a7024337ec
Samuel Beland-Leblanc <samuel.beland.leblanc@gmail>
2019-04-18 16:56:19 -0400

    setup added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python_utilities',
+    version='1.0',
+    packages=['stats'],
+    url='',
+    license='',
+    author='Samuel Beland-Leblanc',
+    author_email='samuel.beland.leblanc@gmail.com',
+    description='Different pyhton utility modules'
+)
