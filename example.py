1ce640edc5bfa421c2f97bbdfd40849594dadfca
Samuel Beland-Leblanc <samuel.beland.leblanc@gmail>
2019-06-26 11:15:11 -0400

    Adjusting fce parsing

--- example.py
+++ example.py
@@ -1,5 +1,10 @@
 import numpy as np
 from python_utilities.stats import PowersetBuilder
+from python_utilities.parsers.fce import extract_original_text_to_file, extract_original_text
+
+
+extract_original_text_to_file('lol.txt', '/home/samuel/Documents/Corpus/FCE/fce-released-dataset/dataset')
+
 
 x = np.array([0.04, 0.16, 0.5, 0.3])
 pb = PowersetBuilder(x, root_size=3)
--- python_utilities/parsers/fce.py
+++ python_utilities/parsers/fce.py
@@ -1,4 +1,6 @@
 from lxml import etree
+from os import walk, path
+
 
 def extract_original_text(file):
     doc = etree.parse(file)
@@ -13,8 +15,24 @@ def extract_original_text(file):
         for ns in p:
             #if the correction is a replacement or a deletion, the first child is a <i> node with the erronuous text
             if ns[0].tag == 'i':
-                s.append(ns[0].text)
+                if ns[0].text:
+                    s.append(ns[0].text)
+                else:
+                    print('nested?')
             #Need to append the tail since <NS> tags are inline
             s.append(ns.tail)
         sentences.append(''.join(s))
     return '\n'.join(sentences)
+
+
+def extract_original_text_to_file(output_fn, dataset_folder, recursive=True):
+    files = []
+    for dpath, _, fnames in walk(dataset_folder):
+        files.extend([path.join(dpath, fn) for fn in fnames])
+        if not recursive:
+            break
+    with open(output_fn, 'w') as out_file:
+        for f in files:
+            out_file.write(extract_original_text(f))
+
+
